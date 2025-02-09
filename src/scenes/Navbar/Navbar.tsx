c55fe1d7545a489cb2d429db73e010a88d19a3d4
Hinyi <mrmck16@gmail.com>
2025-02-09 23:19:05 +0100

    Added home/about/contact

--- src/scenes/Navbar/Navbar.tsx
+++ src/scenes/Navbar/Navbar.tsx
@@ -7,13 +7,20 @@ const Navbar = () => {
   return (
     <>
       <nav className="bg-neutral-700 border-b border-neutral-500">
-        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto px-4 sm:px-6 lg:px-9">
           <div className="flex h-20 items-center justify-between">
             <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
               {/* <ModeToggle /> */}
-              <NavLink to="/" className="flex items-center">
+              <NavLink to="/" className="flex flex-shrink-0 items-center mr-4">
                 <img className="h-8 w-auto" src={logo} alt="Workflow" />
               </NavLink>
+              <div className="md:ml-auto">
+                <div className="flex space-x-2">
+                  <NavLink to={"/"}>Home</NavLink>
+                  <NavLink to={"/about"}>About</NavLink>
+                  <NavLink to={"/contact"}>Contact</NavLink>     
+                </div>
+              </div>
             </div>
           </div>
         </div>
